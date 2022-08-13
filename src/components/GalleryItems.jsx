b3a6444ae8d1fd1949ce8ea48a4f1e93795ba9b2
Your Name <you@example.com>
2022-08-12 23:31:38 -0700

    add css and pass it to both viewstates within the function GalleryItems

--- src/components/GalleryItems.jsx
+++ src/components/GalleryItems.jsx
@@ -1,14 +1,31 @@
 import { useState } from 'react'
 
 function GalleryItem(props) {
-
     //define two view states one for at a glance and one for more detailed once clicked
     //in return statement have a ternary operator when true show detailed and when false show simple view
     let [view, setView] = useState(false)
 
+const simpleStyle = {
+    'width': '25vw',
+    'height': '20vh',
+    'border': '1px solid black',
+    'margin': '2px'
+}
+
+const detailStyle = {
+    'width': '80vw',
+    'height': '20vh',
+    'border': '1px solid black',
+    'margin': '2px',
+    'backgroundImage': `url(${props.item.artworkUrl100})`,
+    'backgroundRepeat': 'no-repeat',
+    'backgroundSize': 'cover',
+    'color': 'yellow'
+}
     const simpleView = () => {
+        <div style ={simpleStyle} />
         return (
-            <div>
+            <div style ={simpleStyle}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
             </div>
@@ -17,7 +34,7 @@ function GalleryItem(props) {
 
     const detailView = () => {
         return (
-            <div>
+            <div style ={detailStyle}>
                 <h2>{props.item.trackName}</h2>
                 <h3>{props.item.collectionName}</h3>
                 <h4>{props.item.primaryGenreName}</h4>
