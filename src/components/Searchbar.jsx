57dcf382a5e459ed0724135dd4619b6cf98e645a
Your Name <you@example.com>
2022-08-12 23:47:15 -0700

    made the search bar search library without pressing prseeing submit

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -5,12 +5,15 @@
      let [searchTerm, setSearchTerm] = useState('')
 //implement the prop info into the function for functionality
 //function that will submit and an event will handle the search based on the search term.
-//will also have an event change which will take the searched term and make it the target value
+//will also have an event change which will take the searched term and make it the target value\
+// in order to have user type without pressing submit we need to comment out the onSubmit function.. change our onChange function to accept what the onSubmit function did.
+
+
      return (
-         <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
- 
+         // <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+            <form>
              <input type="text" placeholder="Enter a search term here"
-             onChange={(e) => setSearchTerm(e.target.value)} />
+             onChange={(e) => props.handleSearch(e, e.target.value)} />
  
              <input type="submit" />
  
