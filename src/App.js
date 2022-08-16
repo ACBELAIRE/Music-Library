0a7bf61e25c91691e23cdc29bcc07da4bcc190a7
Your Name <you@example.com>
2022-08-16 00:32:08 -0700

    remove useffect and took the fetch data informatio and placed it into handlesearch

--- src/App.js
+++ src/App.js
@@ -14,26 +14,22 @@ const App = () => {
 //add a conditional to prevent the function from running continuously if there is no search term
     const API_URL = 'https://itunes.apple.com/search?term='
 
-    useEffect(() => {
-        if(search) {
-            const fetchData = async () => {
-                document.title = `${search} music`
-                const response = await fetch(API_URL + search)
-                const resData = await response.json()
-                if (resData.results.length > 0) {
-                    return setData(resData.results)
-                } else {
-                    return setMessage('Not Found.')
-                }
-            }
-            fetchData()
-        }
-    }, [search])
 
      //prescribe a function to pass down to the searchbar
     // passing two argumentss one is the default event object and the other the term we will type in the search bar
     const handleSearch = (e, term) => {
         e.preventDefault()
+        const fetchData = async () => {
+            document.title = `${term} music`
+            const response = await fetch(API_URL + term)
+            const resData = await response.json()
+            if (resData.results.length > 0) {
+                return setData(resData.results)
+            } else {
+                return setMessage('Not Found.')
+            }
+        }
+        fetchData()
         setSearch(term)
     }
  //ship the data down the the Gallery Component so that it will be able to render in future steps
