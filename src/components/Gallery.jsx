96a6b997facb0eedc386c1d1b0a7a04c12b3e010
Your Name <you@example.com>
2022-08-23 02:27:48 -0700

    Built Link for each part of library. one for the first click and then one for the actual song page

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,9 @@
-import { useContext } from 'react'
-import { DataContext } from '../context/DataContext'
+
 import GalleryItem from './GalleryItems'
 
-function Gallery(){
-    const data = useContext(DataContext)
+function Gallery(props){
     
-    const display = data.map((item,index) => {
+    const display = props.data.map((item,index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
